66259ef621a0721c221b9aaf2d2bcf44d76599f3
Brian Machado <bmachado@live.ca>
2024-01-03 18:29:33 -0500

    Update MANIFEST.in and setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 
 setup(
     name='fastask',
+    packages=['fastask'],
     version='0.2',
     py_modules=['ask'],
-    package_data={'': ['Modelfile']},
     include_package_data=True,
+    package_data={'fastask': ['Modelfile']},
     install_requires=[
         'openai',
         'configparser',
